30c245dcd09a0405c2a99654f2d007632b2fd044
kikikanik <kiki.kanik@gmail.com>
2023-03-05 10:20:25 -0500

    TVC to DVC of reports - precautionary

---
+++ MUPD-SOS/ReportVC/ReportTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReportTableViewCell.swift
+//  MUPD-SOS
+//
+//  Created by Kinneret Kanik on 05/03/2023.
+//
+
+import UIKit
+
+class ReportTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
